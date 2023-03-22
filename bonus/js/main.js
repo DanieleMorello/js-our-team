b0d08247ae992131f1ac1aca3ebc7ff8b56170d5
DanieleMorello <danielemorello@live.it>
2023-03-22 21:41:52 +0100

    Add responsive

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -15,61 +15,76 @@ Stampare le stesse informazioni su DOM sottoforma di stringhe
 // MILESTONE 0
 const team = [
   {
-    nome: 'Wayne Barnett',
+    nome: 'Wayne <br> Barnett',
     ruolo: 'Founder & CEO',
     foto: '../assets/img/wayne-barnett-founder-ceo.jpg'
   },
   {
-    nome: 'Angela Caroll',
+    nome: 'Angela <br> Caroll',
     ruolo: 'Chief Editor',
     foto: '../assets/img/angela-caroll-chief-editor.jpg'
   },
   {
-    nome: 'Walter Gordon',
+    nome: 'Walter <br> Gordon',
     ruolo: 'Office Manager',
     foto: '../assets/img/walter-gordon-office-manager.jpg'
   },
   {
-    nome: 'Angela Lopez',
+    nome: 'Angela <br> Lopez',
     ruolo: 'Social Media Manager',
     foto: '../assets/img/angela-lopez-social-media-manager.jpg'
   },
   {
-    nome: 'Scott Estrada',
+    nome: 'Scott <br> Estrada',
     ruolo: 'Developer',
     foto: '../assets/img/scott-estrada-developer.jpg'
   },
   {
-    nome: 'Barbara Ramos',
+    nome: 'Barbara <br> Ramos',
     ruolo: 'Graphic Designer',
     foto: '../assets/img/barbara-ramos-graphic-designer.jpg'
   }
 ]
 
-// MILESTONE 1
+// Seleziona il contenitore di tutte le colonne
+const containerEl = document.querySelector('.container')
+
+// Contatore per tenere traccia del numero di carte già inserite nella riga corrente
+let cardsInRow = 0
+
+// Crea una nuova riga per la prima carta
+let newRow = document.createElement('div')
+newRow.className = 'row'
+containerEl.appendChild(newRow)
+
 for (let i = 0; i < team.length; i++) {
   const member = team[i]
-  // console.log(member['nome'], member['ruolo'], member['foto'])
-
-  // MILESTONE 2:
-// Seleziona l' elemento col
-const colEl = document.querySelector('.col-3')
-
-// Crea l' elemento card
-const newCard = document.createElement('div.card')
-
-// Aggiunge contenuto
-newCard.innerHTML += `<h2>Nome: ${member.nome} </h2> <h3>Ruolo: ${member.ruolo}</h3>` 
-console.log(newCard);
-// Crea il tag img
-const newDiv = document.createElement('div')
-
-// Aggiunge contenuto
-newDiv.innerHTML += `<img src="${member.foto}" alt="">`
-console.log(newDiv)
-// Stampa
-newCard.appendChild(newDiv)
-
-// Stampa
-colEl.appendChild(newCard)
+  
+  // Se abbiamo già inserito tre carte nella riga corrente, creiamo una nuova riga
+  if (cardsInRow >= 3) {
+    newRow = document.createElement('div')
+    newRow.className = 'row'
+    containerEl.appendChild(newRow)
+    cardsInRow = 0
+  }
+  
+  // Crea una nuova colonna per la carta corrente
+  const newCol = document.createElement('div')
+  newCol.className = 'col'
+  
+  // Crea la nuova carta
+  const newCard = document.createElement('div')
+  newCard.className = 'card'
+  newCard.innerHTML += `<h2>${member.nome} </h2> <h3>${member.ruolo}</h3>`
+  
+  const newDiv = document.createElement('div')
+  newDiv.className = 'img-container'
+  newDiv.innerHTML += `<img src="${member.foto}" alt="">`
+  
+  newCard.appendChild(newDiv)
+  
+  newCol.appendChild(newCard)
+  newRow.appendChild(newCol)
+  
+  cardsInRow++
 }
